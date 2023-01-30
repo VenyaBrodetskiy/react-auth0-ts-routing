a832a5f81bbb1849d3630f42bb1bb3c656adfc78
Venya Brodetskiy <benjamincheg@gmail.com>
2023-01-30 21:18:37 +0200

    added mini-server to repo

---
+++ mini-server-auth0/app.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import { students } from "./data/data";
+import { IStudent } from "./interfaces";
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/api/roles", async (req: Request, res: Response) => {
+
+    const token = process.env.AUTH0_MANAGEMENT_API_TOKEN;
+    const userId = req.query.userId;
+    const endpoint = `https://dev-jz8i8ymmqw7p0b8t.us.auth0.com/api/v2/users/${userId}/roles`;
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+    }
+    let response = await axios.get(endpoint, options);
+    if (response.status === 200 && response.data.length > 0) {
+        console.log(response.data);
+        res.send(response.data);
+        res.end();
+    } else {
+        res.send("no roles found");
+        res.end();
+    }
+});
+
+app.get("/api/students", async (req: Request, res: Response) => {
+    res.send(students);
+})
+
+app.post("/api/students", async (req: Request, res: Response) => {
+    const student = req.body as IStudent;
+    student.id = students.length + 1;
+    students.push(student);
+    res.send(student);
+})
+
+app.listen(3030, () => {
+    console.log("Server started on port 3030");
+});
---
+++ mini-server-auth0/data/data.ts
@@ -0,0 +1,29 @@
+import { IStudent } from "./../interfaces";
+export const students: IStudent[] = [
+    {
+        id: 1,
+        firstName: "John",
+        lastName: "Doe",
+        email: "some@email.com"
+    }, {
+        id: 2,
+        firstName: "John2",
+        lastName: "Doe2",
+        email: "some2@email.com"
+    }, {
+        id: 3,
+        firstName: "John3",
+        lastName: "Doe3",
+        email: "some3@email.com"
+    }, {
+        id: 4,
+        firstName: "John4",
+        lastName: "Doe4",
+        email: "some4@email.com"
+    }, {
+        id: 5,
+        firstName: "John5",
+        lastName: "Doe5",
+        email: "some5@email.com"
+    }
+];
